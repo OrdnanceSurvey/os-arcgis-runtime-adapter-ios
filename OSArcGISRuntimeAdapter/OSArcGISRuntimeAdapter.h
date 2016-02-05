2b201b011ff935eae992d4b7dc4239423d434a3e
drhaynes <drhmbp@gmail.com>
2016-02-05 12:11:16 +0000

    Initial commit.

---
+++ OSArcGISRuntimeAdapter/OSArcGISRuntimeAdapter.h
@@ -0,0 +1,17 @@
+//
+//  OSArcGISRuntimeAdapter.h
+//  OSArcGISRuntimeAdapter
+//
+//  Created by David Haynes on 03/02/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OSArcGISRuntimeAdapter.
+FOUNDATION_EXPORT double OSArcGISRuntimeAdapterVersionNumber;
+
+//! Project version string for OSArcGISRuntimeAdapter.
+FOUNDATION_EXPORT const unsigned char OSArcGISRuntimeAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSArcGISRuntimeAdapter/PublicHeader.h>
---
+++ OSArcGISRuntimeAdapterTests/OSArcGISRuntimeAdapterTests.m
@@ -0,0 +1,28 @@
+//
+//  OSArcGISRuntimeAdapterTests.m
+//  OSArcGISRuntimeAdapterTests
+//
+//  Created by David Haynes on 03/02/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OSArcGISRuntimeAdapterTests : XCTestCase
+
+@end
+
+@implementation OSArcGISRuntimeAdapterTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+}
+
+@end
