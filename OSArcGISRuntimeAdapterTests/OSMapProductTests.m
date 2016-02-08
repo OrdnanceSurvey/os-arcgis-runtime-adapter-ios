64d329f30cb0db6cee1691f23f4b2f51432d26aa
drhaynes <drhmbp@gmail.com>
2016-02-08 20:12:12 +0000

    Add OS product tests.

--- OSArcGISRuntimeAdapterTests/OSMapProductTests.m
+++ OSArcGISRuntimeAdapterTests/OSMapProductTests.m
@@ -15,14 +15,6 @@
 
 @implementation OSMapProductTests
 
-- (void)setUp {
-    [super setUp];
-}
-
-- (void)tearDown {
-    [super tearDown];
-}
-
 - (void)testWellKnownIDIsReturnedCorrectlyForBNG {
     NSInteger wkidBNG = WkIDFromOSSpatialReference(OSSpatialReferenceBNG);
     expect(wkidBNG).to.equal(27700);
@@ -31,6 +23,46 @@
 - (void)testWellKnownIDIsReturnedCorrectlyForWebMercator {
     NSInteger wkidBNG = WkIDFromOSSpatialReference(OSSpatialReferenceWebMercator);
     expect(wkidBNG).to.equal(3857);
+}
+
+- (void)testProductNameIsReturnedCorrectlyForRoadStyleInBNG {
+    NSString *name = NSStringFromOSMapLayer(OSBaseMapStyleRoad, OSSpatialReferenceBNG);
+    expect(name).to.equal(@"Road%2027700");
+}
+
+- (void)testProductNameIsReturnedCorrectlyForOutdoorStyleInBNG {
+    NSString *name = NSStringFromOSMapLayer(OSBaseMapStyleOutdoor, OSSpatialReferenceBNG);
+    expect(name).to.equal(@"Outdoor%2027700");
+}
+
+- (void)testProductNameIsReturnedCorrectlyForLightStyleInBNG {
+    NSString *name = NSStringFromOSMapLayer(OSBaseMapStyleLight, OSSpatialReferenceBNG);
+    expect(name).to.equal(@"Light%2027700");
+}
+
+- (void)testProductNameIsReturnedCorrectlyForNightStyleInBNG {
+    NSString *name = NSStringFromOSMapLayer(OSBaseMapStyleNight, OSSpatialReferenceBNG);
+    expect(name).to.equal(@"Night%2027700");
+}
+
+- (void)testProductNameIsReturnedCorrectlyForRoadStyleInWebMercator {
+    NSString *name = NSStringFromOSMapLayer(OSBaseMapStyleRoad, OSSpatialReferenceWebMercator);
+    expect(name).to.equal(@"Road%203857");
+}
+
+- (void)testProductNameIsReturnedCorrectlyForOutdoorStyleInWebMercator {
+    NSString *name = NSStringFromOSMapLayer(OSBaseMapStyleOutdoor, OSSpatialReferenceWebMercator);
+    expect(name).to.equal(@"Outdoor%203857");
+}
+
+- (void)testProductNameIsReturnedCorrectlyForLightStyleInWebMercator {
+    NSString *name = NSStringFromOSMapLayer(OSBaseMapStyleLight, OSSpatialReferenceWebMercator);
+    expect(name).to.equal(@"Light%203857");
+}
+
+- (void)testProductNameIsReturnedCorrectlyForNightStyleInWebMercator {
+    NSString *name = NSStringFromOSMapLayer(OSBaseMapStyleNight, OSSpatialReferenceWebMercator);
+    expect(name).to.equal(@"Night%203857");
 }
 
 @end
